55325854f01c31ac6876cd35a16be67a8b1c7dc1
dev-dong-su <fkdlrj1230@gmail.com>
2022-11-01 03:06:27 +0900

    feat: MainPage Header 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Header from "../components/common/Header";
 import Responsive from "../components/common/Responsive";
 import SampleContainer from "../containers/sample/SampleContainer";
 
 const MainPage = () => {
-  return <Responsive>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-      <SampleContainer/>
-    </h1>
-  </Responsive>
-}
+  return (
+    <Responsive>
+      <Header></Header>
+      <h1 className='text-3xl font-bold underline'>
+        Hello world!
+        <SampleContainer />
+      </h1>
+    </Responsive>
+  );
+};
 
 export default MainPage;
