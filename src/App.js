ee8fdf065c0e0648f9e7d0a4b1dd05b6f68d56d2
dev-dong-su <fkdlrj1230@gmail.com>
2022-11-01 15:16:14 +0900

    refactor:  app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
 
-const router = createBrowserRouter([{
-  path: "/", element: <MainPage/>,
-}, {
-  path: "/login", element: <LoginPage/>
-}]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
 
 function App() {
-  return (<RouterProvider router={router}>
-  </RouterProvider>);
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {configureStore} from '@reduxjs/toolkit'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./modules";
-import {Provider} from 'react-redux';
+import { Provider } from "react-redux";
 
 const store = configureStore({
   reducer: rootReducer,
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode>
-  <Provider store={store}>
-    <App/>
-  </Provider>
-</React.StrictMode>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
