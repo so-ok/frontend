3e7af70f7245b7550240464ea0c31853a780a781
dev-dong-su <fkdlrj1230@gmail.com>
2022-11-02 19:35:24 +0900

    feat: Loading Component 추가

---
+++ src/components/common/Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+import tw from "tailwind-styled-components";
+
+import loading from "../../assets/Images/loading.png";
+
+const Container = tw.section`flex justify-center items-center`;
+
+const GrowLoading = tw.img`animate-spin w-10 h-10`;
+
+const Loading = () => (
+  <Container aria-label='현재 로딩 중 화면입니다'>
+    <GrowLoading src={loading} alt='Loading'></GrowLoading>
+  </Container>
+);
+
+export default Loading;
