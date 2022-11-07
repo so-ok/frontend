75330c9c1618f34a4eb3334872267eb7f2e6b714
dev-dong-su <fkdlrj1230@gmail.com>
2022-11-07 18:24:45 +0900

    chore: PillList 디자인 수정

--- src/lib/api/pills.js
+++ src/lib/api/pills.js
@@ -1,48 +1,129 @@
 const pills = [
   {
     id: 1,
-    name: '종근당 바이오',
-    ingredient: {
-      프로바이오틱스: '10억 CFU',
-      루테인: '존나많이',
-    },
-    imge: '',
+    name: '대웅제약 에너씨슬 유산균 밀크씨슬 유산균 30캡슐',
+    ingredient: [
+      {
+        name: '실리마린',
+        amount: '130',
+        unit: 'mg',
+      },
+      {
+        name: '프로바이오틱스',
+        amount: '3000000000',
+        unit: 'CFU',
+      },
+    ],
+    attentions: [
+      {
+        name: '간 건강',
+      },
+    ],
   },
   {
     id: 2,
-    name: '종근당 바이오',
-    ingredient: {
-      프로바이오틱스: '10억 CFU',
-      루테인: '존나많이',
-    },
-    imge: '',
+    name: '대웅제약 에너씨슬 유산균 밀크씨슬 유산균 30캡슐',
+    ingredient: [
+      {
+        name: '실리마린',
+        amount: '130',
+        unit: 'mg',
+      },
+      {
+        name: '프로바이오틱스',
+        amount: '3000000000',
+        unit: 'CFU',
+      },
+    ],
+    attentions: [
+      {
+        name: '간 건강',
+      },
+    ],
   },
   {
     id: 3,
-    name: '종근당 바이오',
-    ingredient: {
-      프로바이오틱스: '10억 CFU',
-      루테인: '존나많이',
-    },
-    imge: '',
+    name: '대웅제약 에너씨슬 유산균 밀크씨슬 유산균 30캡슐',
+    ingredient: [
+      {
+        name: '실리마린',
+        amount: '130',
+        unit: 'mg',
+      },
+      {
+        name: '프로바이오틱스',
+        amount: '3000000000',
+        unit: 'CFU',
+      },
+    ],
+    attentions: [
+      {
+        name: '간 건강',
+      },
+    ],
   },
   {
     id: 4,
-    name: '종근당 바이오',
-    ingredient: {
-      프로바이오틱스: '10억 CFU',
-      루테인: '존나많이',
-    },
-    imge: '',
+    name: '대웅제약 에너씨슬 유산균 밀크씨슬 유산균 30캡슐',
+    ingredient: [
+      {
+        name: '실리마린',
+        amount: '130',
+        unit: 'mg',
+      },
+      {
+        name: '프로바이오틱스',
+        amount: '3000000000',
+        unit: 'CFU',
+      },
+    ],
+    attentions: [
+      {
+        name: '간 건강',
+      },
+    ],
   },
   {
     id: 5,
-    name: '종근당 바이오',
-    ingredient: {
-      프로바이오틱스: '10억 CFU',
-      루테인: '존나많이',
-    },
-    imge: '',
+    name: '대웅제약 에너씨슬 유산균 밀크씨슬 유산균 30캡슐',
+    ingredient: [
+      {
+        name: '실리마린',
+        amount: '130',
+        unit: 'mg',
+      },
+      {
+        name: '프로바이오틱스',
+        amount: '3000000000',
+        unit: 'CFU',
+      },
+    ],
+    attentions: [
+      {
+        name: '간 건강',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: '대웅제약 에너씨슬 유산균 밀크씨슬 유산균 30캡슐',
+    ingredient: [
+      {
+        name: '실리마린',
+        amount: '130',
+        unit: 'mg',
+      },
+      {
+        name: '프로바이오틱스',
+        amount: '3000000000',
+        unit: 'CFU',
+      },
+    ],
+    attentions: [
+      {
+        name: '간 건강',
+      },
+    ],
   },
 ];
 
