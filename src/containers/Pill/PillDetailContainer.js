af63d46d3817fc04598a0d266faf1bde5b930f99
dev-dong-su <fkdlrj1230@gmail.com>
2022-11-24 15:18:17 +0900

    :truck: Pill, Category 관련 파일 이동

---
+++ src/containers/Pill/PillDetailContainer.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import PillDetailPage from '../../pages/PillDetailPage';
+import Loading from '../../components/common/Loading';
+import { getPillDetail } from '../../modules/pillDetail';
+
+
+const PillDetailContainer = () => {
+  const dispatch = useDispatch();
+  const { pillDetail, pillListError, loading } = useSelector(
+    ({ pillDetail, pillListError, loading }) => ({
+      loading: loading['pills/GET_PILL'],
+      pillDetail: pillDetail.pillDetail,
+      pillListError: pillDetail.pillListError,
+    }),
+  );
+  const params = useParams();
+
+  useEffect(() => {
+    dispatch(getPillDetail(params.id));
+  }, []);
+
+  useEffect(() => {
+    if (pillListError) {
+      console.error(pillListError);
+    }
+  }, [pillListError]);
+
+  return (
+    !loading ? (
+      <PillDetailPage pillDetail={pillDetail}></PillDetailPage>
+    ) : (
+      <Loading></Loading>
+    )
+  );
+};
+
+export default PillDetailContainer;
---
+++ src/containers/Pill/PillListContainer.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addListAction } from '../../modules/pills';
+
+import Loading from '../../components/common/Loading';
+import PillListComponent from '../../components/Pill/PillListComponent';
+
+const PillListContainer = () => {
+  const dispatch = useDispatch();
+  const { pillList, pillListError, loading, category } = useSelector(
+    ({ pillList, loading, category }) => ({
+      pillList: pillList.list,
+      pillListError: pillList.pillListError,
+      loading: loading['pills/GET_LIST'],
+      category: category.category,
+    }),
+  );
+
+  useEffect(() => {
+    dispatch(addListAction(category));
+  }, [dispatch, category]);
+
+  useEffect(() => {
+    if (pillListError) {
+      console.error(pillListError);
+    }
+  }, [pillListError]);
+
+  return !loading ? (
+    <PillListComponent pillList={pillList}></PillListComponent>
+  ) : (
+    <Loading></Loading>
+  );
+};
+
+export default PillListContainer;
